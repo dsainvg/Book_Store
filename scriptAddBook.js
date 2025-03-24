1b249205330b2db1dd857d5211bdf5a533640293
Durga Sai <dsainvg.20.12.24@kgpian.iitkgp.ac.in>
2025-03-24 19:41:23 +0530

    Feature : Add Books completed

--- scriptAddBook.js
+++ scriptAddBook.js
@@ -1,42 +1,45 @@
-function toggleHomecategories() {
-    document.getElementById("home").classList.toggle("seen");
-  } 
-  window.onclick = function(e) {
-    if (!e.target.matches('.symbol')) {
-    var home = document.getElementById("home");
-      if (home.classList.contains('seen')) {
-        home.classList.remove('seen');
-      }
-    }
-}
 let Books;
-try {
+try {description
     Books = localStorage.getItem("Books");
     Books = JSON.parse(Books);
     console.log(Books,623);
 } catch (error) {
-    Books ={};
+    Books =[];
 }
 
 function addbooksubmit(){
     let bookname = document.getElementById("bookname").value;
     let author = document.getElementById("author").value;
-    let isbn = document.getElementById("isbn").value;
+    let  = document.getElementById("description").value;
     let price = document.getElementById("Price").value;
     let imgUrl = document.getElementById("imgUrl").value;
     console.log(bookname,author,price,imgUrl);
+    len = Books.length;
     let book = {
+        id : len+1,
         bookname: bookname,
         bookauthor: author,
         bookprice: price,
-        bookimgUrl: imgUrl
+        bookimgUrl: imgUrl,
+        description: description
     };
-    if (Books[isbn]) {
-        alert("Book already exists");
-        return;
-    }
-    Books[isbn] = book;
+    Books.push(book);
     localStorage.Books = JSON.stringify(Books);
+    document.getElementById("booksContainer").innerHTML += `
+    <div class="book" id="book${len}">
+         <img src="${book.coverImage}"
+            alt="${book.title}">
+         <div>
+            <p class="title">${book.title}</p>
+            <p class="author">${book.author}</p>
+            <p class="description">${book.description}</p>
+            <p class="price">₹${book.price}</p>
+            <div class="actions">
+               <button class="buy ${cart.find((item)=>item.id == book.id)?"added":""}" onclick="addToCart(event, ${book.id})">${cart.find((item)=>item.id == book.id)?"Added to cart":"Add to cart"}</button>
+               <button class="delete" onclick="removeBook(${book.id})">Delete</button>
+            </div>
+         </div>
+      </div>
+      `;
     console.log(Books);
 }  
-// localStorage.Books = {};
