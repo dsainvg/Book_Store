f22016a409711740c1c5d3097cda8528415048d8
Praneet Dixit <praneetdixit1234@gmail.com>
2025-03-25 09:42:32 +0530

    Feature:Add book modal

--- scriptAddBook.js
+++ scriptAddBook.js
@@ -1,44 +1,29 @@
-let books;
-try {description
-    books = localStorage.getItem("books");
-    books = JSON.parse(books);
-} catch (error) {
-    books =[];
-}
-
-function addbooksubmit(){
+function addbooksubmit(e){
+    e.preventDefault();
     let bookname = document.getElementById("bookname").value;
     let author = document.getElementById("author").value;
     let description = document.getElementById("description").value;
-    let price = document.getElementById("Price").value;
+    let price = document.getElementById("price").value;
     let imgUrl = document.getElementById("imgUrl").value;
-    console.log(bookname,author,price,imgUrl);
-    len = books.length;
+    let len = books.length;
     let book = {
         id : len+1,
-        bookname: bookname,
-        bookauthor: author,
-        bookprice: price,
-        bookimgUrl: imgUrl,
+        title: bookname,
+        author: author,
+        price: price,
+        coverImage: imgUrl,
         description: description
     };
     books.push(book);
-    localStorage.Books = JSON.stringify(Books);
-    document.getElementById("booksContainer").innerHTML += `
-    <div class="book" id="book${len}">
-         <img src="${book.coverImage}"
-            alt="${book.title}">
-         <div>
-            <p class="title">${book.title}</p>
-            <p class="author">${book.author}</p>
-            <p class="description">${book.description}</p>
-            <p class="price">₹${book.price}</p>
-            <div class="actions">
-               <button class="buy ${cart.find((item)=>item.id == book.id)?"added":""}" onclick="addToCart(event, ${book.id})">${cart.find((item)=>item.id == book.id)?"Added to cart":"Add to cart"}</button>
-               <button class="delete" onclick="removeBook(${book.id})">Delete</button>
-            </div>
-         </div>
-      </div>
-      `;
-}  
+    localStorage.setItem("books", JSON.stringify(books));
+    showBooks();
+    document.getElementById("addForm").reset();
+}
 
+document.getElementById("addbook").addEventListener("click", function(){
+    document.getElementById("newBook").classList.toggle("active");
+});
+
+document.getElementById("closeModal").addEventListener("click", function(){
+    document.getElementById("newBook").classList.toggle("active");
+});
